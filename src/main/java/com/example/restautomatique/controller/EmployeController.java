058b1c65743758d9fffc266b922883ec0d19aa3d
Mohamed <mohameddrame907@gmail.com>
2023-02-08 23:10:54 +0100

    remove unused spaces

--- src/main/java/com/example/restautomatique/controller/EmployeController.java
+++ src/main/java/com/example/restautomatique/controller/EmployeController.java
@@ -51,16 +51,11 @@ public class EmployeController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-
-
-
         columnName.setCellValueFactory(new PropertyValueFactory<Employe, String>("name"));
         columnAge.setCellValueFactory(new PropertyValueFactory<Employe, String>("age"));
         columnJob.setCellValueFactory(new PropertyValueFactory<Employe, String>("role"));
         columnWorkHours.setCellValueFactory(new PropertyValueFactory<Employe, String>("hour"));
         tableEmployee.setItems(employeesModels);
-
-//        ObservableList<Book> books = FXCollections.observableArrayList();
 
         btnAdd.setOnMousePressed( actionEvent -> {
             String filedNameValue = filedName.getText();
@@ -70,26 +65,19 @@ public class EmployeController implements Initializable {
 
             boolean conditionAdd = (!Objects.equals(filedNameValue, "") && !Objects.equals(fieldAgeValue, "") && !Objects.equals(fieldJobValue, "") && !Objects.equals(fieldWorkHoursValue, ""));
 
-
             if (conditionAdd) {
                 Employe new_employe = new Employe(filedNameValue, Integer.parseInt(fieldAgeValue), fieldJobValue, Integer.parseInt(fieldWorkHoursValue));
-                // Employe new_employe = new Employe("filedNameValue", 2, "fieldJobValue", 3);
                 employeesModels.add(new_employe);
             } else if (Objects.equals(fieldWorkHoursValue, "")) {
                 System.out.println(fieldAge.getText());
-                 Employe new_employe = new Employe(filedName.getText(), Integer.parseInt(fieldAgeValue), fieldJobValue, 0);
-               // Employe new_employe = new Employe("defaut 0", 2, "fieldJobValue", 3);
-                System.out.println(new_employe);
-
+                Employe new_employe = new Employe(filedName.getText(), Integer.parseInt(fieldAgeValue), fieldJobValue, 0);
                 employeesModels.add(new_employe);
             }
         });
     }
 
-
     private ObservableList<Employe> employeesModels = FXCollections.observableArrayList(
             new Employe ("moi",12, "Chepchieng", 2),
             new Employe("mm",2, "Too", 3)
     );
-
 }
