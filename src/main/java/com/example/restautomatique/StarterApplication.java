e5ce04cf9d66538da0d0fa579fb815a32c0f75b0
Mohamed <mohameddrame907@gmail.com>
2023-02-08 10:51:34 +0100

    setup/add fxml new file and mvc's on

--- src/main/java/com/example/restautomatique/StarterApplication.java
+++ src/main/java/com/example/restautomatique/StarterApplication.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class StarterApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(StarterApplication.class.getResource("MainView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Restautomatique");
         stage.setScene(scene);
         stage.show();
     }
