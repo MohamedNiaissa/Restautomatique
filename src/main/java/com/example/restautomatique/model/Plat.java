ecbddc7f9598dd7e881c291d017f59c324ce08d0
David ROQUAIN <david.roquainpro@gmail.com>
2023-02-09 07:35:35 +0100

    refactor: change class Plat for testing

--- src/main/java/com/example/restautomatique/model/Plat.java
+++ src/main/java/com/example/restautomatique/model/Plat.java
@@ -1,16 +1,13 @@
 package com.example.restautomatique.model;
-
-import java.util.ArrayList;
-
 public class Plat {
     private String name;
     private String description;
-    private ArrayList<String> ingredient;
+    private String ingredient;
     private int sellPrice;
     private int preparationPrice;
     private String picture;
 
-    public Plat(String name, String description, int sellPrice, int preparationPrice, String picture, ArrayList<String> ingredient) {
+    public Plat(String name, String description, int sellPrice, int preparationPrice, String picture, String ingredient) {
         this.name = name;
         this.description = description;
         this.sellPrice = sellPrice;
@@ -35,11 +32,11 @@ public class Plat {
         this.description = description;
     }
 
-    public ArrayList<String> getIngredient() {
+    public String getIngredient() {
         return ingredient;
     }
 
-    public void setIngredient(ArrayList<String> ingredient) {
+    public void setIngredient(String ingredient) {
         this.ingredient = ingredient;
     }
 
